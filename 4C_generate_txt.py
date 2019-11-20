206a356d201c079e6e42dd7d837a2008f272353d
André <andre@fuchsborst.de>
2019-11-20 19:41:17 +0100

    GitHub Pages Init

--- 4C_generate_txt.py
+++ 4C_generate_txt.py
@@ -6,18 +6,16 @@ Feel free to customize the padding to the left and right sides of the kerning pa
 import json
 
 # TO DO:
-# add stylistic alternates for QUOATES and en-/em-DASHES (maybe for (false) apostrophes, too)
-# QUOTES = [ '‚', '„', '“', '”', '‘', '’', '«', '»', '‹', '›', '"', "'", ]
-# Kombinatorik; Alle Ziffern + mathematische Zeichen + Klammern
+# Generate all combinations of numbers, math characters and brackets
 
 
 with open("count/fonts/googleFontsKernVectorList.json", "r") as input_json, open(
     "result/relevant_kerning_with_padding.txt", "w"
 ) as outputPadded, open(
-    "result/relevant_kerning_raw.txt", "w"
+    "result/relevant_kerning_raw.json", "w", encoding="utf8"
 ) as outputRaw:
     relevantGlobalKerningPairs = json.load(input_json)
     kerningPadded = ["nonu" + v[0] + "nuou\n" for v in relevantGlobalKerningPairs]
-    kerningList =  [v[0] + " " for v in relevantGlobalKerningPairs]  # better .join() instead?
+    kerningList =  [v[0] for v in relevantGlobalKerningPairs]
     outputPadded.writelines(kerningPadded)
-    outputRaw.writelines(kerningList)
+    outputRaw.write(json.dumps(kerningList, indent=4, ensure_ascii=False))
